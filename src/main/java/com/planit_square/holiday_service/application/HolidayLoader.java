8595b01071716522179201dc634fdfb409e7569c
horse <kjh47914@gmail.com>
2025-12-07 19:00:06 +0900

    fix: 외부 API 조회 병렬 처리 로그 수정

--- src/main/java/com/planit_square/holiday_service/application/HolidayLoader.java
+++ src/main/java/com/planit_square/holiday_service/application/HolidayLoader.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,27 +33,49 @@ public class HolidayLoader {
     public void initialize() {
 
         Instant start = Instant.now();
-
         log.info("### 초기화 시작 ###");
 
+        // 국가 초기 데이터 적재
+        List<RegisterCountryCommand> countryCommands = initializeCountries();
+
+        // TODO: 고민 필요 -> 예외처리, 재시도
+        // 공휴일 초기 데이터 적재
+        initializeHolidays(countryCommands);
+
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        log.info("### 초기화 완료 - 소요 시간: {}분 {}초", duration.toMinutes(), duration.toSecondsPart());
+    }
+
+    private List<RegisterCountryCommand> initializeCountries() {
         List<RegisterCountryCommand> countryCommands = holidayKeeper.findCountries();
         countryCommandUseCase.initialize(countryCommands);
-
         log.info("### 전체 국가 수: {} 건", countryCommands.size());
+        return countryCommands;
+    }
 
-        // TODO: 고민 필요 -> 병렬 처리, 예외처리
+    private void initializeHolidays(List<RegisterCountryCommand> countryCommands) {
+
         List<RegisterHolidayCommand> allHolidayCommands = Collections.synchronizedList(new ArrayList<>());
 
-        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL);
+        AtomicInteger successCount = new AtomicInteger();
+        AtomicInteger failCount = new AtomicInteger();
+
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL);   // 5
 
         try {
             List<CompletableFuture<Void>> futures = new ArrayList<>();
-
             for (RegisterCountryCommand countryCommand : countryCommands) {
-
                 CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                    List<RegisterHolidayCommand> holidayCommands = holidayKeeper.findHolidays(countryCommand.code());
-                    allHolidayCommands.addAll(holidayCommands);
+                    try {
+                        List<RegisterHolidayCommand> holidayCommands = holidayKeeper.findHolidays(countryCommand.code());
+                        allHolidayCommands.addAll(holidayCommands);
+                        successCount.incrementAndGet();
+                        log.info("### 국가코드: {} - 성공 수: {} / 전체 국가 수: {}", countryCommand.code(), successCount.get(), countryCommands.size());
+                    } catch (Exception e) {
+                        failCount.incrementAndGet();
+                        log.info("### 국가코드: {} - 실패 수: {} / 전체 국가 수: {}", countryCommand.code(), failCount.get(), countryCommands.size());
+                    }
                 }, executor);
 
                 futures.add(future);
@@ -63,12 +86,6 @@ public class HolidayLoader {
             if (!allHolidayCommands.isEmpty()) {
                 holidayCommandUseCase.initialize(allHolidayCommands);
             }
-
-            Instant end = Instant.now();
-            Duration duration = Duration.between(start, end);
-
-            log.info("### 초기화 완료 - 소요 시간: {}분 {}초", duration.toMinutes(), duration.toSecondsPart());
-
         } finally {
             executor.shutdown();
         }
